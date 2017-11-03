54a73fda6bee44c4ac263c0b998fa096a6c75e56
pengteling <pengteling@gmail.com>
2017-11-03 21:13:12 +0800

    1103

---
+++ SectionListBasics.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+
+export default class SectionListBasics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'D', data: [{key:2,name:'Devin'}]},
+            {title: 'J', data: [{key:1,name:'Jackson'} ,{key:3,name:'John'}]},//key报错
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+          renderSectionHeader={({section}) => <Text key={section.title} style={styles.sectionHeader}>{section.title}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('rn1', () => SectionListBasics);
